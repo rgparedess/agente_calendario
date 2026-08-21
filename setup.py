ab225822c06f95417e0e7cf80ec97ec9cfd52d6a
Reinel G. Paredes <reinelgparedes@gmail.com>
2026-08-21 12:01:11 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,12 +3,16 @@
 
 from setuptools import setup, find_packages
 
+import os
+
+long_description = ""
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-with open("CHANGELOG.md", "r", encoding="utf-8") as fh:
-    changelog = fh.read()
-    long_description += "\n\n---\n\n## Historial de Cambios\n\n" + changelog
+if os.path.exists("CHANGELOG.md"):
+    with open("CHANGELOG.md", "r", encoding="utf-8") as fh:
+        changelog = fh.read()
+        long_description += "\n\n---\n\n## Historial de Cambios\n\n" + changelog
 
 setup(
     name="agente_calendario",
